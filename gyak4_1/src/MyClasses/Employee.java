5f8c47f2515945f17d767c9490d628cc2f46ee1d
baloghbianka <baloghbianka@localhost>
2021-03-03 13:26:48 +0100

    4. gyakorlat 1. feladat 'c' verzió

--- gyak4_1/src/MyClasses/Employee.java
+++ gyak4_1/src/MyClasses/Employee.java
@@ -1,16 +1,59 @@
 package MyClasses;
 
 public class Employee {
-	String name;
-	int salary;
-
-	void increaseSalary(int value) {
+	private String name;
+	private int salary;
+	//a,
+	public void increaseSalary(int value) {
 		salary += value;
 	}
 	
-	String displayInfo() {
+	public String displayInfo() {
 		//increaseSalary(100);
-		return "Név: " + name + ", fizetés: " + salary;
+		return "Név: " + name + ", fizetés: " + salary +
+				", adó: " + getTax();
+	}
+
+	
+	//b,
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		if(salary>0)
+			this.salary = salary;
+	}
+	
+	//c,
+	public boolean isInRange(int lower, int upper){
+		if(this.salary <= upper && this.salary >= lower)
+			return true;
+		return false;
+	}
+	
+	private double getTax() {
+		return Math.round(this.salary*0.16);
+	}
+	
+	public boolean hasHigherSalary(Employee e) {
+		if(this.salary > e.salary)
+			return true;
+		return false;
+	}
+	
+	public Employee compareSalary(Employee e) {
+		if(this.salary > e.salary)
+			return this;
+		return e;
 	}
 	
 }
--- gyak4_1/src/test/EmployeeTest.java
+++ gyak4_1/src/test/EmployeeTest.java
@@ -1,10 +1,29 @@
 package test;
 
+import MyClasses.Employee;
+
 public class EmployeeTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Employee emp1 = new Employee ();
+		emp1.setName( "John");
+		emp1.setSalary(10000);
+		
+		Employee emp2 = new Employee ();
+		emp2.setName( "Jane");
+		emp2.setSalary(15000);
+		
+		System.out.println(emp1.displayInfo());
+		System.out.println(emp2.displayInfo());
+		
+		boolean res =emp1.hasHigherSalary(emp2);
+			if (res)
+				System.out.println(emp1.getName()+ " fizetése nagyobb");
+			else
+				System.out.println(emp1.getName()+ " fizetése kisebb");
+		
+		System.out.println(emp2.compareSalary(emp1).getName());
 	}
 
 }
